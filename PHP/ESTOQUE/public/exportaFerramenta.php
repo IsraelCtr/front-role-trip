157b8b08094243aed890ebb8c72a04154c744039
Israel Castro <94873358+IsraelCtr@users.noreply.github.com>
2025-06-16 12:12:13 -0300

    remove mkdir php

--- PHP/ESTOQUE/public/exportaFerramenta.php
+++
@@ -1,7 +0,0 @@
-<?php
-$ferramenta=[
-    'nome'=> $_POST ['nome'],
-    'marca'=> $_POST ['marca'], 
-    'valor'=> $_POST ['valor'], 
-    'unidade'=> $_POST ['unidade']];
-file_put_contents('../ferramenta.json',json_encode($ferramenta));
--- PHP/ESTOQUE/src/acrescentaInsumo.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once "C:/Users/israe\OneDrive/Documentos/repositorio/PHP/ESTOQUE/src/model/insumo.php";
-$Insumo = new Insumo('',0);
-$Insumo -> acrescentaInsumo();
